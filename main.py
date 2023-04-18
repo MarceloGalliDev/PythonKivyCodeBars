90d11b78d85070d65cc366ad3ddec060698606eb
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-17 21:38:24 -0300

    update
    update

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+
+GUI = Builder.load_file('tela.kv')
+
+class MeuApp (App):
+  def build(self):
+    return GUI
